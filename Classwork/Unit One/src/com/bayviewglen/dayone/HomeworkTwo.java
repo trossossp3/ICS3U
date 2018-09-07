512537bfd972c2ab9eba5813b02b9a0d854c0098
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2018-09-07 15:23:39 -0400

    some homework

--- Classwork/Unit One/src/com/bayviewglen/dayone/HomeworkTwo.java
+++ Classwork/Unit One/src/com/bayviewglen/dayone/HomeworkTwo.java
@@ -1,8 +1,14 @@
 package com.bayviewglen.dayone;
 
+
 public class HomeworkTwo {
 	
 	public static void main(String[] args) {
+		System.out.println("   O  ");
+		System.out.println("   |  ");
+		System.out.println("------");
+		System.out.println("   |  ");
+		System.out.println("  /\\ ");
 		
 	}
 
