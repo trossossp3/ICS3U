6559e211da7211a20a0c063e7a2f2c7f2a6eeb60
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2018-09-08 10:50:25 -0400

    Finished Homework

---
+++ Classwork/Unit One/src/com/bayviewglen/dayone/HomeworkFour.java
@@ -0,0 +1,18 @@
+package com.bayviewglen.dayone;
+
+public class HomeworkFour {
+	public static void main(String[] args) {
+		System.out.println("/////////////////////////////////////////////////////////////////\r\n" + 
+				"/								/\r\n" + 
+				"/		   Our Lady of Mercy Hospital	                /\r\n" + 
+				"/		   -------------------------------------	/\r\n" + 
+				"/					                        /\r\n" +    
+				"/			    1306 Central City	                /\r\n" + 
+				"/		        Phone (416) 555-1212			/\r\n" + 
+				"/								/\r\n" + 
+				"/////////////////////////////////////////////////////////////////\r\n" + 
+				"\r\n" + 
+				"");
+	}
+
+}
---
+++ Classwork/Unit One/src/com/bayviewglen/dayone/HomeworkThree.java
@@ -0,0 +1,16 @@
+package com.bayviewglen.dayone;
+
+public class HomeworkThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("275 Duncan Mill Road\r\n" + 
+				"Toronto, Ontario M3B 3H9\r\n" + 
+				"Canada\r\n" + 
+				"Phone: 416.443.1030\r\n" + 
+				"Fax: 416.443.1032");
+
+	}
+
+}
