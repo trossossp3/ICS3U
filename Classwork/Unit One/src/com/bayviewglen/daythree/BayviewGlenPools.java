53377374b8d106cd00d1f4b42b2997ef52bd2505
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2018-09-12 11:46:39 -0400

    continued work on assesmeny.
    the length of trans is wrong u need to use pythagoris

--- Classwork/Unit One/src/com/bayviewglen/daythree/BayviewGlenPools.java
+++ Classwork/Unit One/src/com/bayviewglen/daythree/BayviewGlenPools.java
@@ -7,6 +7,18 @@ public class BayviewGlenPools {
 		double deepDepth =0;
 		double shallowLength = 0;
 		double transLength =0 ;
+		double price = 0;
+		double deepLength = length - shallowLength - transLength;
+		double transHeight = deepDepth - shallowDepth;
+		
+		double deepVolume = deepDepth * deepLength;
+		double shallowVolume = shallowLength * shallowDepth;
+		double transVolume = (transLength * deepDepth) - ((length - shallowLength - deepLength)*transHeight)/2;
+		double volume = transVolume + shallowVolume + deepVolume;
+		
+		
+		double deepSA = (deepDepth * deepLength) * 2; //not finished
+		
 		
 	}
 
