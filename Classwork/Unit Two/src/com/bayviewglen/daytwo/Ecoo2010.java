4b19abfa4c44f0e4efd8c4258bc644757b8ee7eb
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2018-10-25 11:30:18 -0400

    class work

---
+++ Classwork/Unit Two/src/com/bayviewglen/daytwo/Ecoo2010.java
@@ -0,0 +1,18 @@
+package com.bayviewglen.daytwo;
+
+import java.util.Scanner;
+
+public class Ecoo2010 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int m = in.nextInt();
+		int n = in.nextInt();
+		int p = in.nextInt();
+		int q = in.nextInt();
+		
+		
+		
+	}
+
+}
